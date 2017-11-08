84f040679a909828a0bf9c9079203f3bf0372795
Yassin Siouda <yassin@siouda.com>
2017-11-08 12:39:30 +0100

    feat: Posts database structure

---
+++ database/migrations/2017_11_06_194952_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('type');
+            $table->string('title');
+            $table->string('body');
+            $table->string('video_url');
+            $table->string('image_path');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
